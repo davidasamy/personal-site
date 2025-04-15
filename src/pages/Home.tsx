a63d37023c6de2622d5f0db012308884014774eb
David Samy <96505813+davidasamy@users.noreply.github.com>
2025-04-14 17:22:22 -0700

    preload video

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
+
 function Home() {
+    useEffect(() => {
+        const link = document.createElement('link');
+        link.rel = 'preload';
+        link.as = 'video';
+        link.href = '/sleap-viewer.mov';
+        link.type = 'video/quicktime'; // or 'video/mp4' if it's .mp4
+        document.head.appendChild(link);
+
+        return () => {
+            document.head.removeChild(link);
+        };
+    }, []);
+
     return (
-        <div className="home-container no-scrollbar">
+        <div className="home-container">
             <h1>I'm David Samy</h1>
             <p>
                 Currently a student at the University of Washington. I'm interested in neuroscience, computer vision, robotics, and math.
