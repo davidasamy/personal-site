698b63811cc0b47b0c309e2861698a204263547d
David Samy <96505813+davidasamy@users.noreply.github.com>
2025-04-22 21:19:00 -0700

    add robotics

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -5,10 +5,17 @@ function Work() {
         <div>
             <h1>work</h1>
 
+            <FloatingVideo src="/robots.mp4" />
+
+            <div className="proj-desc">
+                <p>vision-based targeting system for detecting and shooting enemies. developed as part of the UW’s competitive robotics team.
+                </p>
+            </div>
+
             <FloatingVideo src="/sleap-viewer.mov" />
 
             <div className="proj-desc">
-                <p>pose viewer developed to demonstrate capability of package I helped develop (sleap-io). coded in python, compiled to webassembly, to run in the browser!</p>
+                <p>interactive pose viewer for sleap-io, a tool I helped develop at the salk institute. built in python and compiled to webassembly for fast performance.</p>
             </div>
 
             {/* <FloatingVideo src="/sleap-viewer.mov" />
